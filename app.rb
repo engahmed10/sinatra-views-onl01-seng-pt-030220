22c6342b60db89d2d408c49f28f1c66d074accbc
AHMED AFANDY <eng.computer1@yahoo.com>
2020-06-30 07:41:54 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,6 +1,7 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+<<<<<<< HEAD
 
 
  get '/'  do
@@ -11,5 +12,16 @@ class App < Sinatra::Base
       erb :info
 
  end
+=======
+	get '/' do
+    "<h1>Hello World</h1>"
+  end
+
+
+ get '/info'  do
+    erb :index
+ end
+
+>>>>>>> b4a9fea80738b94dc356830d73301306fd0300cd
 
 end
